c172bea19db17c4ef6da71031608b26c41c8e9a0
Booozz <blody.ti@googlemail.com>
2016-06-28 17:15:00 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require( 'gulp-util' );
+var gnf = require('gulp-npm-files');
+var rename = require("gulp-rename");
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var ftp = require( 'vinyl-ftp' );
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
+
+//  development
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: "./src",
+    });
+});
+gulp.task('modules:get', function() {
+    return gulp.src(gnf(), {base:'./'})
+    .pipe(gulpIf('*.css', gulp.dest('./src/css/')));
+});
+gulp.task('modules:build', function() {
+    return gulp.src("./src/css/node_modules/**/*.css")
+    .pipe(rename(function (path) { path.extname = ".css";}))
+    .pipe(gulp.dest("./src/css/modules"));
+});
+gulp.task('modules', function (callback) {
+    return runSequence('clean:modules','modules:get','modules:build','clean:node_modules',callback);
+});
+gulp.task('sass', function () {
+    gulp.src('./src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./src/css/'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+});
+gulp.task('watch', ['browserSync', 'sass'], function () {
+    gulp.watch('./src/sass/**/*.scss', ['sass'])
+    gulp.watch('./src/*.html', browserSync.reload)
+    gulp.watch('./src/pages/*.html', browserSync.reload)
+    gulp.watch('./src/js/**/*.js', browserSync.reload)
+});
+
+//  production
+
+gulp.task('clear:cache', function (callback) {
+    return cache.clearAll(callback);
+});
+gulp.task('useref', function(){
+    return gulp.src(['./src/pages/*.html','./src/index.html'])
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulpIf('!*.html', gulp.dest('./dist')))
+    .pipe(gulpIf(['*.html','!index.html'], gulp.dest('./dist/pages')))
+    .pipe(gulpIf('index.html', gulp.dest('./dist')));
+});
+gulp.task('images', function(){
+    return gulp.src('./src/assets/images/*.+(png|jpg|gif)')
+    .pipe(cache(imagemin({
+        interlaced: true
+    })))
+    .pipe(gulp.dest('dist/assets/images'));
+});
+gulp.task('icons', function() {
+    return gulp.src('./src/assets/icons/*')
+    .pipe(gulp.dest('dist/assets/icons'));
+});
+gulp.task('fonts', function() {
+    return gulp.src('./src/assets/fonts/**/*')
+    .pipe(gulp.dest('dist/assets/fonts'));
+});
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+gulp.task('clean:modules', function() {
+    return del.sync('./src/css/lib');
+});
+gulp.task('clean:node_modules', function() {
+    del.sync('./src/css/node_modules');
+});
+
+
+gulp.task( 'deploy', function () {
+
+    var conn = ftp.create( {
+        host:     'wp204.webpack.hosteurope.de',
+        user:     'ftp1177004-portfolio',
+        password: 'pen!s_13',
+        parallel: 10,
+        log:      gutil.log
+    } );
+
+    var globs = [
+        'dist/**'
+    ];
+
+    return gulp.src( globs, { base: '.', buffer: false } )
+        .pipe( conn.newer( '/' ) )
+        .pipe( conn.dest( '/' ) );
+} );
+
+
+
+
+//  sequences
+
+gulp.task('default', function (callback) {
+    runSequence('modules',['sass','browserSync','watch'],callback);
+});
+gulp.task('build', function (callback) {
+    runSequence('clean:dist','modules',['sass','useref','images','icons','fonts'],callback);
+});
---
+++ node_modules/gulp-npm-files/README.md
@@ -0,0 +1,69 @@
+# gulp-npm-files
+
+Gulp plugin for listing package.json dependencies
+
+## Get started
+
+### Install
+
+```
+npm install gulp-npm-files
+```
+
+### Examples
+
+```javascript
+var gulp = require('gulp');
+var gnf = require('gulp-npm-files');
+
+// Copy dependencies to build/node_modules/
+gulp.task('copyNpmDependenciesOnly', function() {
+  gulp.src(gnf(), {base:'./'}).pipe(gulp.dest('./build'));
+});
+
+// Copy dependencies and devDependencies to build/node_modules/
+gulp.task('copyAllNpmDependencies', function() {
+  gulp.src(gnf(true), {base:'./'}).pipe(gulp.dest('./build'));
+});
+
+// Copy dependencies to build/node_modules/ by
+// value in './path/to/package.json' file
+gulp.task('copyNpmDependenciesAtDifferentFolder', function() {
+  gulp
+    .src(gnf(null, './path/to/package.json'), {base:'./'})
+    .pipe(gulp.dest('./build'));
+});
+
+// Copy dependencies and devDependencies to build/node_modules/ by
+// value in './path/to/package.json' file
+gulp.task('copyAllNpmDependenciesAtDifferentFolder', function() {
+  gulp
+    .src(gnf(true, './path/to/package.json'), {base:'./'})
+    .pipe(gulp.dest('./build'));
+});
+
+```
+## LICENSE
+
+(MIT License)
+
+Copyright (c) 2015 Nipher <github@nipher.com.br>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
